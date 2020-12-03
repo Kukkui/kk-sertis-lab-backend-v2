e1a40536210893bacf1ae8333b70a481eabe0ed2
Punyawee Posri <pposr@sertiscorp.com>
2020-12-03 04:40:42 +0000

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 'use strict';
 const express = require('express');
-const routes = express.Router;
+const routes = require('./routes');
 const bodyParser = require('body-parser');
 const dbConfig = require('./configs/development.config.js');
 const mongoose = require('mongoose');
